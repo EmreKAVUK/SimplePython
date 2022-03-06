1ac8802dc4bdaab6180431be682f5363f3c1bf64
Emre KAVUK <61249520+EmreKAVUK@users.noreply.github.com>
2022-03-06 16:37:58 +0300

    Update translate.py

--- Translator/translate.py
+++ Translator/translate.py
@@ -1,12 +1,6 @@
 from googletrans import Translator
-a = True
-while a:
-    text= input("Enter Your Message : ")
-    if text == "exit" or text == "e":
-        a = False
-    else:
-        translator = Translator()
-        dt = translator.detect(text)
-        translated = translator.translate(text)
-        print(translated.text)
-
+text="Naber kanka"
+translator = Translator()
+dt = translator.detect(text)
+translated = translator.translate(text)
+print(translated.text)
